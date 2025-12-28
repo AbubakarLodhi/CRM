0c7ba526f4bed4a0c79158a2d826580eb0f943a6
zeeshan <zeeshanbutt223@gmail.com>
2025-12-28 15:36:40 +0500

    customer enttity updated + customer seeder

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,27 +4,53 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Customer extends Model
 {
     use HasUuids;
 
-    protected $fillable = ['merchant_id','name','phone','email','city','reference_id'];
+    /** @var bool $incrementing */
     public $incrementing = false;
+
+    /** @var string[] $fillable */
+    protected $fillable = [
+        'merchant_id', 'name', 'phone', 'email', 'city', 'reference', 'country_id', 'city_id', 'postal_code', 'address',
+    ];
+
+    /** @var string $keyType */
     protected $keyType = 'string';
 
-    public function merchant()
+    /**
+     * @return BelongsTo
+     */
+    public function merchant(): BelongsTo
     {
         return $this->belongsTo(Merchant::class);
     }
-    public function reference()
+
+    /**
+     * @return BelongsTo
+     */
+    public function reference(): BelongsTo
     {
         return $this->belongsTo(Customer::class, 'reference_id');
     }
 
-    public function referencedBy()
+    /**
+     * @return BelongsTo
+     */
+    public function country(): BelongsTo
     {
-        return $this->hasMany(Customer::class, 'reference_id');
+        return $this->belongsTo(Country::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'city_id');
     }
 
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
             StaffsSeeder::class,
             BusinessesSeeder::class,
             BranchesSeeder::class,
-            BranchUsersSeeder::class
+            BranchUsersSeeder::class,
+            CustomersSeeder::class,
         ];
 
         $this->call($seeders);
